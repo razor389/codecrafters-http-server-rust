5285342dcb1a2926f105c4ae08488aa8f6c6fda4
razor389 <rgranowski@gmail.com>
2024-09-16 11:25:16 -0500

    200 response

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
 #[allow(unused_imports)]
 use std::net::TcpListener;
+use std::io::{Read, Write};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
+    // Bind the listener to the address and port
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
+    // Handle incoming connections
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                println!("Accepted new connection");
+
+                // Create a buffer to read the incoming request
+                let mut buffer = [0; 512];
+                stream.read(&mut buffer).unwrap();
+
+                // Prepare a basic HTTP 200 OK response
+                let response = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n";
+                
+                // Write the response to the stream
+                stream.write(response.as_bytes()).unwrap();
+                stream.flush().unwrap();
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("Error: {}", e);
             }
         }
     }
